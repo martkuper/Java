389c9c7f8c9376ffe8bea5a2843dbbc3ac08f4b0
Mart Kuper <martkuper@hotmail.com>
2013-11-19 16:08:06 +0100

    Added constructor and getter for birth date

--- src/tutoraat/week01/opgave02/Persoon.java
+++ src/tutoraat/week01/opgave02/Persoon.java
@@ -1,5 +1,14 @@
 import java.util.Arrays;
 
+/**
+ * Class voor personen
+ *
+ * @author Mart Kuper
+ * @version 1.0
+ * Last edit 19-11-2013
+ */
+
+
 public class Persoon {
 
     private int BSN;
@@ -11,18 +20,57 @@ public class Persoon {
     private char gender;
     private int[] monthsWith31Days = {1, 3, 5, 7, 8, 10, 12};
 
-    public Persoon(int aBSN, String aFirstName, String aLastName, int aBDay, int aMDay, int aYDay, char aGender){
-
-
-
+    /**
+     * Maak een persoon met de gegeven waarden
+     *
+     * @param aBSN  Je burgerservicenummer
+     * @param aFirstName Je voornaam
+     * @param aLastName Je achternaam
+     * @param aDay Je geboortedag
+     * @param aMonth Je geboortemaand
+     * @param aYear Je geboortejaar
+     * @param aGender Je geslacht
+     */
+   public Persoon(int aBSN, String aFirstName, String aLastName, int aDay, int aMonth, int aYear, char aGender){
+        BSN = aBSN;
+        firstName = aFirstName;
+        lastName = aLastName;
+        gender = aGender;
+        setBirthDate(aDay, aMonth, aYear);
     }
 
+    /**
+     * Getter voor de geboortedatum
+     * @return Geboortedatum
+     */
+    public String getGeboorteDatum(){
+        String temp;
+        if(bDay == 0 && bMonth == 0 && bYear == 0){
+            temp = "Onbekend";
+        }else{
+            temp = bDay + "/" + bMonth + "/" + bYear;
+        }
+
+        return temp;
+    }
+
+    /**
+     * Zet je geboortedag, maand en jaar op 0.
+     */
     private void invalidDate(){
         bDay = 0;
         bMonth = 0;
         bYear = 0;
     }
 
+    /**
+     * Methode om je geboortedatum te veranderen
+     * aan de hand van de ingevoerden dag, maand en jaar
+     *
+     * @param aDay Je geboortedag
+     * @param aMonth Je geboortemaand
+     * @param aYear Je geboortejaar
+     */
     public boolean setBirthDate(int aDay, int aMonth, int aYear){
         if(aDay < 1 || aMonth < 1 || aMonth > 12 || aYear < 1900 || aYear > 2100){
 
